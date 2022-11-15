2a74ed140b50dccd1c46a4f23c5f4a39021a1891
Apple <riyabulia12@gmail.com>
2022-11-15 15:09:13 +0530

    Cleanup and fixed linters

--- app.rb
+++ app.rb
@@ -27,6 +27,25 @@ class App
     puts "Thank you for using this app!\n\n" if user_response == '9'
   end
 
+  def add_person
+    person = Person.create_person
+    @people << person
+    puts "\n\nPerson created successfully" if @people.include?(person)
+  end
+
+  def add_book
+    book = Book.create_book
+    @books << book
+    puts "\n\n Book added successfully" if @books.include?(book)
+  end
+
+  def add_rental
+    rental = Rental.create_rental(@books, @people)
+    @rentals << rental
+    puts "\n\n Rental created successfully" if @rentals.include?(rental)
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
   def check_selection(response)
     case response
     when '0'
@@ -38,34 +57,16 @@ class App
     when '3'
       Teacher.lists_all_teachers(@people)
     when '4'
-      person = Person.create_person
-      @people << person
-      puts "\n\nPerson created successfully" if @people.include?(person)
+      add_person
     when '5'
-      book = Book.create_book
-      @books << book
-      puts "\n\n Book added successfully" if @books.include?(book)
-
+      add_book
     when '6'
-      rental = Rental.create_rental(@books, @people)
-      @rentals << rental
-      puts "\n\n Rental created successfully" if @rentals.include?(rental)
+      add_rental
     when '7'
       Person.list_all_rentals_person_id(@people, @rentals)
     when '8'
       Book.list_all_rentals_for_book(@books, @rentals)
     end
   end
-
-  # def list_all_books
-  #   @books.each do |book|
-  #     puts "Title: \"#{book.title}\" Author: #{book.author}"
-  #   end
-  # end
-
-  # def list_all_people
-  #   @people.each do |person|
-  #     puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-  #   end
-  # end
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
--- main.rb
+++ main.rb
@@ -6,16 +6,18 @@ def main
   app.run
 end
 
-def app_options = [
-  '[0]  =>  List all Books',
-  '[1]  =>  List all People',
-  '[2]  =>  List all Students',
-  '[3]  =>  List all Teachers',
-  '[4]  =>  Create a Person',
-  '[5]  =>  Create a Book',
-  '[6]  =>  Create a Rental',
-  '[7]  =>  List all Rentals for a given Person',
-  '[8]  =>  List all Rentals for a given Book',
-  '[9]  =>  Exit'
-]
+def app_options
+  [
+    '[0]  =>  List all Books',
+    '[1]  =>  List all People',
+    '[2]  =>  List all Students',
+    '[3]  =>  List all Teachers',
+    '[4]  =>  Create a Person',
+    '[5]  =>  Create a Book',
+    '[6]  =>  Create a Rental',
+    '[7]  =>  List all Rentals for a given Person',
+    '[8]  =>  List all Rentals for a given Book',
+    '[9]  =>  Exit'
+  ]
+end
 main
