8eae63dae92fceae0f992080d1d2636c234b2cb4
Apple <riyabulia12@gmail.com>
2022-11-15 18:52:47 +0530

    Update user_input.rb

--- user_input.rb
+++ user_input.rb
@@ -4,27 +4,26 @@ class UserInput
   def self.save_people(people)
     people_json = []
     people.each do |person|
+    p person
     unless person.class.to_s == 'Teacher'
-      person = { 
-        id: person.id.to_s, 
-        name: person.name, 
+      person = {
+        id: person.id.to_s,
+        name: person.name,
         age: person.age,
         classroom: person.classroom,
-        parent_permission: person.parent_permission.to_s
+        parent_permission: person.parent_permission
       }
-      people_json << person
-    else 
-      person = { 
-        id: person.id.to_s, 
-        name: person.name, 
+    else
+      person = {
+        id: person.id.to_s,
+        name: person.name,
         age: person.age,
         specialization: person.specialization,
       }
-
-      people_json << person
     end
-    File.write("people.json", (people_json, {indent: "\t", object_nl: "\n"}))
-    end   
+    people_json << person
+    File.write("people.json", JSON.pretty_generate(people_json))
+    end
   end
 
   def save_books
